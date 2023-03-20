4ed37a842eaae4171d1ccad8dff8754224f5f53a
sumayya othman <sumayyaothman96@gmail.com>
2023-03-20 14:53:43 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line to the standard error
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is usefu\l" - Dora Korpar, 2015-10-19\n" stdout);
+    return (1);
+}
