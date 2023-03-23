ef90c3a2c18c8d922bd8e8b48dc40530b7922c59
sumayya othman <sumayyaothman96@gmail.com>
2023-03-22 17:59:55 -0700

    prints the last digit of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 
 	/**
-	 * _abs - computes the absolute value of an integer
+	 * print__abs - computes the absolute value of an integer
 	 * @n: the int to check
 	 * Return: the absolute value of int
 	 */
